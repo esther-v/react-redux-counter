1f45cef57133ab2ece6ae030e6fa0877f6daa7e6
esther-v <esvalas@gmail.com>
2021-11-16 15:02:21 +0100

    redux devtools

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore, combineReducers} from 'redux'
+import {createStore} from 'redux'
+import allReducers from './reducers';
 
-const store = createStore()
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <React.StrictMode>
