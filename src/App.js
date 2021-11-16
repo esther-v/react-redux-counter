62a34ecb23b8341ea8bbf59fe78245fb75cb4d4a
esther-v <esvalas@gmail.com>
2021-11-16 14:44:50 +0100

    combine reducers

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, {useEffect, useState} from "react"
 import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0)
   return (
     <div>
-      <h1>Counter: {counter}</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}>Increment</button>
+      <h1>Counter: 0</h1>
+      <button>Increment</button>
     </div>
   );
 }
